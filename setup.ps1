ef27dd44bebea827f8ddbf1d8b00269202b44a77
ch <mkmsacc@outlook.com>
2024-03-18 11:06:04 +0700

    In addition, it also downloads the configuration file for GlazeWM directly from a specified url and places it in the appropriate directory, creating the directory if it does not already exist.

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,13 @@
 # install glazewm
 winget install GlazeWM -h --accept-package-agreements --accept-source-agreements
 
+$url = "https://raw.githubusercontent.com/chubbyhippo/.glaze-wm/main/config.yaml"
+$destinationPath = "$HOME\.glaze-wm\"
+if(!(Test-Path $destinationPath)) {
+    New-Item -ItemType Directory -Force -Path $destinationPath
+}
+
+$fullPath = $destinationPath + "config.yaml"
+Invoke-WebRequest -Uri $url -OutFile $fullPath
+
+
