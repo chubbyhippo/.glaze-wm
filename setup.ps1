8ee126a4b65fa9055d3eb607abc257415f2861b9
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-09-07 10:51:47 +0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,3 @@
-# install glazewm
-$glazewmUrl = "https://github.com/glzr-io/glazewm/releases/download/v2.1.1/GlazeWM_x64_2.1.1.exe"
-$installedPath = "$Home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\glazewm.exe"
-Invoke-WebRequest -Uri $glazewmUrl -OutFile $installedPath
-
 $url = "https://raw.githubusercontent.com/chubbyhippo/.glaze-wm/main/config.yaml"
 $destinationPath = "$HOME\.glaze-wm\"
 if(!(Test-Path $destinationPath)) {
